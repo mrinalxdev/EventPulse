5fd53351ef89f977ec6884eaf4dc6bbe5eaf3d20
Mrinal Pramanick <mrinalbitsat@gmail.com>
2023-08-24 15:13:16 +0530

    Added Controllers

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.json())
+
 app.use("/api/contacts", require("./routes/contactRoute"));
 
 app.listen(port, () => {
